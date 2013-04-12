a5c1a607639f064bd4d52f0c5fe00fa32830c06f
Jonas Bengtsson <jonas.b@gmail.com>
2013-04-12 22:40:26 +0200

    Reschedule alarms on boot, time change, time zone change

--- Veckify/src/com/wigwamlabs/veckify/alarms/BroadcastReceiver.java
+++ Veckify/src/com/wigwamlabs/veckify/alarms/BroadcastReceiver.java
@@ -12,11 +12,21 @@ public class BroadcastReceiver extends android.content.BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
         if (intent != null) {
-            if (ACTION_ALARM.equals(intent.getAction())) {
+            final String action = intent.getAction();
+            final AlarmCollection alarmCollection = new AlarmCollection(context);
+            if (ACTION_ALARM.equals(action)) {
                 Debug.logAlarmScheduling("Received alarm broadcast");
                 Toast.makeText(context, "ALARM", Toast.LENGTH_LONG).show();
-
-                final AlarmCollection alarmCollection = new AlarmCollection(context);
+                alarmCollection.rescheduleAlarm();
+            } else if (Intent.ACTION_BOOT_COMPLETED.equals(action)) {
+                Debug.logAlarmScheduling("Device booted");
+                alarmCollection.rescheduleAlarm();
+            } else if (Intent.ACTION_TIME_CHANGED.equals(action)) {
+                Debug.logAlarmScheduling("Time changed");
+                alarmCollection.rescheduleAlarm();
+            } else if (Intent.ACTION_TIMEZONE_CHANGED.equals(action)) {
+                Debug.logAlarmScheduling("Timezone changed");
+                // need to reschedule since alarm is scheduled in UTC
                 alarmCollection.rescheduleAlarm();
             }
         }
