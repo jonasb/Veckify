975ec558dccf32f170674182b6766192ba7629ae
Jonas Bengtsson <jonas.b@gmail.com>
2013-04-12 16:46:33 +0200

    Move JNI code to SpotifyLibrary

--- SpotifyLibrary/jni/Android.mk
+++ SpotifyLibrary/jni/Android.mk
@@ -41,5 +41,5 @@ include $(BUILD_SHARED_LIBRARY)
 #
 include $(CLEAR_VARS)
 LOCAL_MODULE := libspotify
-LOCAL_SRC_FILES := ../vendor/spotify/lib/libspotify.so
+LOCAL_SRC_FILES := ../../vendor/spotify/lib/libspotify.so
 include $(PREBUILT_SHARED_LIBRARY)
