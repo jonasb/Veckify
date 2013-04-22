c4712c99d572f13c7bc6fe4742c6eaae14738f73
Jonas Bengtsson <jonas.b@gmail.com>
2013-04-23 00:35:58 +0200

    Add VeckifyTest module

---
+++ VeckifyTest/src/com/wigwamlabs/veckify/AlarmSchedulingTest.java
@@ -0,0 +1,9 @@
+package com.wigwamlabs.veckify;
+
+import junit.framework.TestCase;
+
+public class AlarmSchedulingTest extends TestCase {
+    public void testFail() {
+        fail("TODO");
+    }
+}
