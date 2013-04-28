a58c966d0475295a797d43673b5cac805695370f
Jonas Bengtsson <jonas.b@gmail.com>
2013-04-28 17:38:51 +0200

    Show notification for scheduled alarm

--- Veckify/src/com/wigwamlabs/veckify/alarms/AlarmCollection.java
+++ Veckify/src/com/wigwamlabs/veckify/alarms/AlarmCollection.java
@@ -1,17 +1,23 @@
 package com.wigwamlabs.veckify.alarms;
 
 import android.app.AlarmManager;
+import android.app.Notification;
+import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
+import android.support.v4.app.NotificationCompat;
 import android.text.format.DateFormat;
 import android.util.Pair;
 
 import com.wigwamlabs.veckify.Debug;
+import com.wigwamlabs.veckify.MainActivity;
+import com.wigwamlabs.veckify.R;
 
 import java.util.Calendar;
+import java.util.Date;
 
 public class AlarmCollection {
     private static final String ALARM_ENABLED = "alarm_enabled";
@@ -27,6 +33,10 @@ public class AlarmCollection {
     private final Context mContext;
     private final AlarmManager mAlarmManager;
     private final Alarm mAlarm;
+    private final NotificationManager mNotificationManager;
+    private final PendingIntent mAlarmPendingIntent;
+    private final java.text.DateFormat mFormatDate;
+    private final java.text.DateFormat mFormatTime;
 
     public AlarmCollection(Context context) {
         mContext = context;
@@ -43,6 +53,12 @@ public class AlarmCollection {
         mAlarm.setShuffle(mPreferences.getBoolean(ALARM_SHUFFLE, false));
 
         mAlarmManager = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
+        mNotificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+
+        mAlarmPendingIntent = PendingIntent.getActivity(mContext, 0, new Intent(mContext, MainActivity.class), 0);
+
+        mFormatDate = DateFormat.getDateFormat(mContext);
+        mFormatTime = DateFormat.getTimeFormat(mContext);
     }
 
     public Alarm getAlarm() {
@@ -93,11 +109,30 @@ public class AlarmCollection {
         final PendingIntent pendingIntent = PendingIntent.getBroadcast(mContext, 0, intent, PendingIntent.FLAG_CANCEL_CURRENT);
 
         if (next != null) {
-            mAlarmManager.set(AlarmManager.RTC_WAKEUP, next.second.getTimeInMillis(), pendingIntent);
             Debug.logAlarmScheduling("Scheduling alarm at " + DateFormat.format("yyyy-MM-dd kk:mm", next.second));
+            mAlarmManager.set(AlarmManager.RTC_WAKEUP, next.second.getTimeInMillis(), pendingIntent);
+            mNotificationManager.notify(R.id.notificationAlarm, getNotification(next.first, next.second));
         } else {
-            mAlarmManager.cancel(pendingIntent);
             Debug.logAlarmScheduling("No next alarm, cancel any existing");
+            mAlarmManager.cancel(pendingIntent);
+            mNotificationManager.cancel(R.id.notificationAlarm);
         }
+    }
+
+    private Notification getNotification(Alarm alarm, Calendar calendar) {
+        final Date time = calendar.getTime();
+        final String description = String.format(mContext.getString(R.string.notification_alarm_description),
+                mFormatDate.format(time),
+                mFormatTime.format(time),
+                alarm.getPlaylistName());
+
+        final NotificationCompat.Builder builder = new NotificationCompat.Builder(mContext)
+                .setSmallIcon(com.wigwamlabs.spotify.R.drawable.ic_stat_alarm)
+                .setPriority(NotificationCompat.PRIORITY_LOW)
+                .setContentIntent(mAlarmPendingIntent)
+                .setContentTitle(mContext.getString(R.string.notification_alarm_title))
+                .setContentText(description)
+                .setOngoing(true);
+        return builder.build();
     }
 }
