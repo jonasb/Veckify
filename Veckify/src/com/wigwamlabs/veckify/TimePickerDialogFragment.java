2e549d1139b9e4e5053b53631f31fc5c14e63f7e
Jonas Bengtsson <jonas.b@gmail.com>
2013-05-19 23:26:23 +0200

    Switch to BetterTimePicker

--- Veckify/src/com/wigwamlabs/veckify/TimePickerDialogFragment.java
+++ Veckify/src/com/wigwamlabs/veckify/TimePickerDialogFragment.java
@@ -1,20 +1,24 @@
 package com.wigwamlabs.veckify;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.app.Dialog;
 import android.app.DialogFragment;
-import android.app.TimePickerDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
-import android.text.format.DateFormat;
-import android.widget.TimePicker;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
 
+import com.doomonafireball.betterpickers.timepicker.TimePicker;
 import com.wigwamlabs.veckify.db.AlarmEntry;
 
 @SuppressWarnings("WeakerAccess")
-public class TimePickerDialogFragment extends DialogFragment implements TimePickerDialog.OnTimeSetListener {
+public class TimePickerDialogFragment extends DialogFragment {
     private static final String ARG_ALARM_ID = "alarmid";
     private static final String ARG_ALARM_ENTRY = "alarmentry";
     private AlarmEntry mEntry;
+    private TimePicker mTimePicker;
 
     static TimePickerDialogFragment create(long alarmId, AlarmEntry entry) {
         final TimePickerDialogFragment fragment = new TimePickerDialogFragment();
@@ -30,27 +34,33 @@ public class TimePickerDialogFragment extends DialogFragment implements TimePick
         final Bundle bundle = getArguments();
         mEntry = bundle.getParcelable(ARG_ALARM_ENTRY);
 
-        int hour = 9;
-        int minute = 0;
-        if (mEntry.getTime() != null) {
-            hour = mEntry.getHour();
-            minute = mEntry.getMinute();
-        }
-
         final Activity activity = getActivity();
-        return new TimePickerDialog(activity, this, hour, minute, DateFormat.is24HourFormat(activity));
+        final View view = LayoutInflater.from(activity).inflate(R.layout.dialog_time, null, false);
+        mTimePicker = (TimePicker) view.findViewById(R.id.time_picker);
+
+        return new AlertDialog.Builder(activity)
+                .setView(view)
+                .create();
     }
 
     @Override
-    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
-        // this method is called twice sometimes by TimePickerDialog,
-        // so check that the value has changed to prevent unnecessary db writes
-        if (mEntry.getTime() == null || hourOfDay != mEntry.getHour() || minute != mEntry.getMinute()) {
-            mEntry.setTime(hourOfDay, minute);
+    public void onDismiss(DialogInterface dialog) {
+        super.onDismiss(dialog);
 
-            final Bundle bundle = getArguments();
-            final long alarmId = bundle.getLong(ARG_ALARM_ID);
-            ((MainActivity) getActivity()).onAlarmEntryChanged(alarmId, mEntry, true);
+        final MainActivity activity = (MainActivity) getActivity();
+        if (activity == null) {
+            // happens if fragment is destroyed, e.g. at screen rotation
+            return;
         }
+
+        final Bundle bundle = getArguments();
+        final long alarmId = bundle.getLong(ARG_ALARM_ID);
+        final AlarmEntry entry = bundle.getParcelable(ARG_ALARM_ENTRY);
+
+        final int mins = mTimePicker.getHours() * 60 + mTimePicker.getMinutes();
+
+        entry.setTime(mins / 60, mins % 60);
+
+        activity.onAlarmEntryChanged(alarmId, entry, true);
     }
 }
